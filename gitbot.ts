d8350e88a8131900887857690cf9614f2221f7a4
willchen90 <willchen90@gmail.com>
2015-11-28 19:52:18 -0800

    refactored query into a class

--- gitbot.ts
+++ gitbot.ts
@@ -1,6 +1,5 @@
-import Git from "./git";
-import Error from "./error";
 import queryGitbot from "./query-gitbot";
+import * as _ from "lodash";
 
 export default function gitbot(query: string): string {
 	const NO_RESPONSE="";
@@ -9,5 +8,48 @@ export default function gitbot(query: string): string {
 		return NO_RESPONSE;
 	}
 	queryWords.shift();
-	return queryGitbot(queryWords.join(" "));
+	let queryString = queryWords.join(" ");
+	let queryInstance = new Query(queryString);
+	return queryGitbot(queryInstance);
+}
+
+class Query {
+	
+	queryString: string;
+	
+	constructor (queryString: string) {
+		this.queryString = queryString;
+	}
+	
+	startsWith (keyword: string | string[]) {
+		if (typeof keyword === "string") {
+			return this.startsWithSingular(keyword);	
+		} else {
+			let results = _.map(keyword, (value) => {
+				return this.startsWithSingular(value);
+			});
+			return _.every(results);
+		}
+	}
+	
+	private startsWithSingular (keyword: string) {
+		let regex = new RegExp("^" + keyword);
+		return regex.test(this.queryString)
+	}
+	
+	includes (keyword: string | string[]) {
+		if (typeof keyword === "string") {
+			return this.includesSingular(keyword);
+		} else {
+			let results = _.map(keyword, (value) => {
+				return this.includesSingular(value)
+			});
+			return _.every(results);
+		}
+	}
+	
+	private includesSingular (keyword: string) {
+		let regex = new RegExp(keyword);
+		return regex.test(this.queryString);
+	}
 }
--- query-gitbot.ts
+++ query-gitbot.ts
@@ -1,12 +1,19 @@
-export default function queryGitbot(query: string): string {
-	if ((/^git add/).test(query)) {
+import Git from "./git";
+import Error from "./error";
+
+export default function queryGitbot(query: Query): string {
+	if (query.startsWith("git add")) {
 		return Git.gitAdd();
 	}
-	if ((/^git remove/).test(query)) {
+	if (query.startsWith("git remove")) {
 		return Git.gitRemove();
 	}
-	if ((/rebase/).test(query) && (/merge/.test(query))) {
+	if (query.includes(["rebase", "merge"])) {
 		return Git.gitPullVsRebase();
 	}
+	if (query.includes("git stash")) {
+		return Git.gitStash();
+	}
+	
 	return Error.doNotUnderstand();
 }
